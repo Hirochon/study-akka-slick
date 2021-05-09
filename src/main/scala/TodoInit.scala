0559dac87bf62814a4cf1bdd815ac689a1a46542
Hirochon <heacet43@gmail.com>
2021-05-10 01:36:53 +0900

    [add] sqlの初期化を追加

--- src/main/scala/TodoInit.scala
+++ src/main/scala/TodoInit.scala
@@ -1,13 +1,36 @@
+import slick.ast.BaseTypedType
+import slick.jdbc.JdbcType
 import slick.jdbc.MySQLProfile.api._
+import slick.sql.SqlProfile.ColumnOption.SqlType
 
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 import scala.concurrent.Await
 import scala.concurrent.duration.Duration
+import scala.language.implicitConversions
 
 object TodoInit extends App{
-  class TodoList(tag: Tag) extends Table[(Int, String)](tag, _tableName="todo_list") {
+
+  val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
+  implicit def LocalDateTimeToStr(d: LocalDateTime): String = {
+    d.format(formatter)
+  }
+  implicit def strToLocalDateTime(s: String): LocalDateTime = {
+    LocalDateTime.parse(s, formatter)
+  }
+  implicit val localDateTimeMapper: JdbcType[LocalDateTime] with BaseTypedType[LocalDateTime] = MappedColumnType.base[LocalDateTime, String](
+    LocalDateTimeToStr,
+    strToLocalDateTime
+  )
+
+  class TodoList(tag: Tag) extends Table[(Int, String, Int, LocalDateTime, Int, LocalDateTime)](tag, _tableName="plan") {
     def id: Rep[Int] = column[Int]("id", O.PrimaryKey, O.AutoInc)
-    def todo: Rep[String] = column[String]("todo")
-    def * = (id, todo)
+    def todo: Rep[String] = column[String]("todo", SqlType("VARCHAR(256)"))
+    def urgency: Rep[Int] = column[Int]("urgency")
+    def deadline: Rep[LocalDateTime] = column[LocalDateTime]("deadline", O.SqlType("datetime"))(localDateTimeMapper)
+    def status: Rep[Int] = column[Int]("status")
+    def createdAt: Rep[LocalDateTime] = column[LocalDateTime]("created_at", O.SqlType("datetime"))(localDateTimeMapper)
+    def * = (id, todo, urgency, deadline, status, createdAt)
   }
 
   val todoList = TableQuery[TodoList]
